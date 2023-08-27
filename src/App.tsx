aa8a8c45cecadc4fb5e93c65a76b63d5e0c0074a
ahita1 <ahitafani833@gmail.com>
2023-08-27 13:07:08 +0300

    Add New Feature

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import ExpenseForm from "./expense-tracker/components/expenseForm";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import Form from "./components/form";
+
+export const Catagories = ["Groceries", "Utilities", "Entertainment"];
+
+const App = () => {
+  const [selectedCatagory, setSelectedCatagory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+  const visibleExpenses = selectedCatagory
+    ? expenses.filter((e) => e.category === selectedCatagory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="mb-5">
+        <Form></Form>
+      </div>
+      <div className="mb-3">
+        <ExpenseForm />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCatagory={(category) => setSelectedCatagory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
+};
+export default App;
